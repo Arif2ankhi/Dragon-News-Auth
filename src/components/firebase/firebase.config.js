7fa315780e5566463d12772581cad24900764858
Arif2ankhi <arif2ankhi@gmail.com>
2024-11-15 08:24:42 +0000

    Added login and registration form

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBH7I1V3KldZQG6AxxJuaI_Hu0Jp4BYejs",
+  authDomain: "dragon-news-23823.firebaseapp.com",
+  projectId: "dragon-news-23823",
+  storageBucket: "dragon-news-23823.firebasestorage.app",
+  messagingSenderId: "220140320336",
+  appId: "1:220140320336:web:c251015a354924b0bac355"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
